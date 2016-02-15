5979ca7482c22333bf95b1982953ea70a5d189fa
Kah Wei <msntkw@msn.com>
2016-02-15 16:38:10 +0800

    Added gamegrid class

---
+++ SP4/Assets/Scripts/GameGrid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    // Grid Size
+    [Tooltip("The number of blocks along the width.")]
+    public int BlocksInWidth = 9;
+    [Tooltip("The number of blocks along the height.")]
+    public int BlocksInHeight = 18;
+
+    // Calculated Scale of Blocks
+    private Vector2 blockScale;
+
+    // Storage of Blocks
+    private List<List<GameObject>> listOfBlocks = new List<List<GameObject>>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Get scale of the grid
+        Vector2 scale = transform.lossyScale;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
