addad6102b6e4ea00b5868e9bfe52bb901d964ef
NothingMuch123 <curious_boii@hotmail.com>
2016-02-15 18:27:29 +0800

    Added SpriteData class that contains function to get sprite's native size. Added ScreenData class that contains functions to get screen size by object such that the object is able to scale to screen size.

---
+++ SP4/Assets/Scripts/ScreenData.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenData : MonoBehaviour
+{
+	public static Vector2 GetAspectRatio()
+	{
+		float f = (float)Screen.width / (float)Screen.height;
+		int i = 0;
+		while (true)
+		{
+			i++;
+			if (System.Math.Round(f * i, 2) == Mathf.RoundToInt(f * i))
+				break;
+		}
+		return new Vector2((float)System.Math.Round(f * i, 2), i);
+	}
+
+	public static Vector2 GetScreenSize() // If object is 1x1 unit in size
+	{
+		/*Bounds b = new Bounds();
+		Vector2 aspect = GetAspectRatio();
+		float orthoSize = Camera.main.orthographicSize;
+
+		b.center = Vector3.zero;
+		b.max = new Vector3(orthoSize / aspect.y * aspect.x, orthoSize);
+		b.min = -b.max;
+		return b;*/
+
+		Vector2 aspect = GetAspectRatio();
+		float orthoSize = Camera.main.orthographicSize;
+		return new Vector2(
+			(orthoSize / aspect.y * aspect.x * 2),
+			(orthoSize * 2)
+			);
+	}
+
+	public static Vector2 GetScreenSizeByObject(Vector2 origSize, float pixelPerUnit) // If object is NOT 1x1 unit in size
+	{
+		/*Bounds b = new Bounds();
+
+		b.center = Vector3.zero;
+		b.max = new Vector3(, orthoSize);
+		b.min = -b.max;*/
+
+		Vector2 aspect = GetAspectRatio();
+		float orthoSize = Camera.main.orthographicSize;
+		return new Vector2(
+			(orthoSize / aspect.y * aspect.x * 2) / (origSize.x / pixelPerUnit),
+			(orthoSize * 2) / (origSize.y / pixelPerUnit)
+			);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- SP4/Assets/Scripts/SpriteData.cs
+++ SP4/Assets/Scripts/SpriteData.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class SpriteData : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +11,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public static Vector2 GetNativeSize(Sprite sprite)
+	{
+		return sprite.textureRect.max;
 	}
 }
