4d84dabdd2f8145c1bb048f4c1340f340f216c84
Kah Wei <msntkw@msn.com>
2016-02-15 15:03:46 +0800

    Added scripts for controlling time, sound and score

---
+++ SP4/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public enum ScoreType
+    {
+        EnemyKill,
+        PowerUpPickUp,
+        PlayerDeath,
+    }
+
+    private static int currentScore;
+    private static int[] scores =
+        {
+            100,
+            50,
+            -1000
+        };
+
+    // Saving and Loading
+    private static string currentScoreKey = "CurrentScore";
+
+    // Getters
+    public static int CurrentScore { get { return currentScore; } }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void AddCurrentScore(ScoreType score)
+    {
+        currentScore += scores[(int)score];
+    }
+
+    public static void ResetCurrentScore()
+    {
+        currentScore = 0;
+    }
+
+    public static void SaveScore()
+    {
+        PlayerPrefs.SetInt(currentScoreKey, currentScore);
+        PlayerPrefs.Save();
+    }
+
+    public static void LoadScore()
+    {
+        if (PlayerPrefs.HasKey(currentScoreKey))
+        {
+            currentScore = PlayerPrefs.GetInt(currentScoreKey);
+        }
+        else
+        {
+            currentScore = 0;
+        }
+    }
+}
---
+++ SP4/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public enum TimeType
+    {
+        Normal,
+        Game
+    }
+
+    private static double[] timeScale = { 1.0, 1.0 };
+
+    public static double GetTimeScale(TimeType type)
+    {
+        return timeScale[(int)type];
+    }
+
+    public static void SetTimeScale(TimeType type, double scale)
+    {
+        if (type == TimeType.Normal)
+        {
+            throw new UnityException("Cannot modify Normal time scale! Use other existing TimeTypes or create a new TimeType instead.");
+        }
+
+        timeScale[(int)type] = scale;
+    }
+
+    public static double GetDeltaTime(TimeType type)
+    {
+        return Time.deltaTime * timeScale[(int)type];
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
