ae554fd0bc232db9be453609f069ca1d4e2de019
unknown <140189H@M313G104.sidm.student.local>
2016-02-15 18:15:02 +0800

    Added SceneManager, where we can swap scenes easily. Added Sticky and Slippery blocks

---
+++ SP4/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadMainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void LoadGame()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void LoadOptions()
+    {
+        Application.LoadLevel("Options");
+    }
+
+    public void LoadCredits()
+    {
+        Application.LoadLevel("Credits");
+    }
+
+    public void LoadShops()
+    {
+        Application.LoadLevel("Shops");
+    }
+
+    public void LoadScores()
+    {
+        Application.LoadLevel("Scores");
+    }
+}
