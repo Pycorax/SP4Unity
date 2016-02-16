78f5d168c6dee47fb0fbda43cd883f241cc0f86f
unknown <140189H@M313G104.sidm.student.local>
2016-02-16 18:02:56 +0800

    Added Weapon Base Class Added Sword Class Revamped Game Idea

---
+++ SP4/Assets/Scripts/Weapons/Sword.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : Weapon {
+
+    public Sprite SwordSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+        setName("Sword");
+        setDamage(10);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Attack()
+    {
+        //Play Swinging Animation
+        //Deal Damage?
+    }
+}
