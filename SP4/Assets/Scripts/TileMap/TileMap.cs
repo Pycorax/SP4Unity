294ff2294f9d7cae156c7687efd54c895b216b5d
NothingMuch123 <curious_boii@hotmail.com>
2016-02-17 18:46:40 +0800

    Added activating tiles by top left and bottom right position

--- SP4/Assets/Scripts/TileMap/TileMap.cs
+++ SP4/Assets/Scripts/TileMap/TileMap.cs
@@ -2,12 +2,24 @@
 using System;
 using System.Collections;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 /**********
 Multi-layer tile system
 NOTE: Writing format is from top to bottom. No spaces allowed
 Example: Tile A is supposed to be rendered above Tile B, writing format is "A|B"
 **********/
+
+public class MultiLayerTile
+{
+    public List<GameObject> multiLayerTile = new List<GameObject>();
+}
+
+public class Row
+{
+    public List<MultiLayerTile> column = new List<MultiLayerTile>();
+}
 
 public class TileMap : MonoBehaviour
 {
@@ -27,21 +39,33 @@ public class TileMap : MonoBehaviour
 		TILE_CENTER,
 		TILE_BOTTOM_LEFT,  
 	};
+    
+    [Tooltip("Tiles blueprint for instantiating.")]
+    public GameObject[] TileBlueprints = new GameObject[(int)Tile.TILE_TYPE.NUM_TILE];
+    [Tooltip("Default tile if no tile is specified.")]
+    public Tile.TILE_TYPE DefaultTile = Tile.TILE_TYPE.TILE_EMPTY;
 
-	// Tiles blueprint for instantiating
-	public GameObject[] TileBlueprints = new GameObject[(int)Tile.TILE_TYPE.NUM_TILE];
-	Tile.TILE_TYPE DefaultTile = Tile.TILE_TYPE.TILE_EMPTY;
+    // Tile map data
+    [Tooltip("Origin point of Tile Map.")]
+    public TILEMAP_ORIGIN TileMapOrigin = TILEMAP_ORIGIN.TILEMAP_CENTER;
+    [Tooltip("Origin point of Tile.")]
+    public TILE_ORIGIN TileOrigin = TILE_ORIGIN.TILE_CENTER;
+    [Tooltip("Path to map file.")]
+    public string Filepath = "";
+    [Tooltip("Number of tile(s) vertically.")]
+    public int NumOfTiles = 9;
 
-	// Tile map data
-	public TILEMAP_ORIGIN TileMapOrigin = TILEMAP_ORIGIN.TILEMAP_CENTER;
-	public TILE_ORIGIN TileOrigin = TILE_ORIGIN.TILE_CENTER;
-	public string Filepath = "";
-	public int NumOfTiles = 9;
 	private Vector2 NumOfScreenTiles = new Vector2();
 	private int TileSize = 32;
+    private Vector3 tileMapDistToTopLeft = new Vector3();
+    private int rowCount, colCount;
 
-	// Map
-	private ArrayList map = new ArrayList();
+
+    private List<Tile> tiles = new List<Tile>();
+
+    // Map
+    //private ArrayList map = new ArrayList();
+    private List<Row> map = new List<Row>();
 
 
 	// Use this for initialization
@@ -50,15 +74,75 @@ public class TileMap : MonoBehaviour
 		if (Filepath != "")
 		{
 			loadFile();
-		}
-		
-	}
+        }
+        //ActivateTiles(0, rowCount - 1, 0, colCount - 1);
+        //ActivateTiles(1, 13, 5, 26);
+        ActivateTiles(map[1].column[5].multiLayerTile[0].transform.position - new Vector3(TileSize * 0.5f, 0.0f), map[13].column[26].multiLayerTile[0].transform.position - new Vector3(TileSize * 0.5f, 0.0f));
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	
 	}
+
+    public MultiLayerTile FetchTile(Vector3 position)
+    {
+        Vector2 tileIndex = FetchTileIndex(position);
+        return FetchTile((int)tileIndex.x, (int)tileIndex.y);
+    }
+
+    public MultiLayerTile FetchTile(int rowIndex, int colIndex)
+    {
+        List<GameObject> result = new List<GameObject>();
+
+        //ArrayList row = (ArrayList)map[rowIndex];
+        //ArrayList multiLayerTile = (ArrayList)row[colIndex];
+
+        Row row = map[rowIndex];
+        MultiLayerTile tiles = row.column[colIndex];
+        return tiles;
+    }
+
+    public Vector2 FetchTileIndex(Vector3 position)
+    {
+        Vector3 posFromTopRight = position + tileMapDistToTopLeft;
+        posFromTopRight.y = Math.Abs(posFromTopRight.y);
+
+        int rowIndex = (int)(posFromTopRight.y / TileSize);//(rowCount * TileSize / posFromTopRight.y);
+        int colIndex = (int)(posFromTopRight.x / TileSize);//(colCount * TileSize / posFromTopRight.x);
+
+        return new Vector2(rowIndex, colIndex);
+    }
+
+    public void ActivateTiles(int rowIndexMin, int rowIndexMax, int colIndexMin, int colIndexMax)
+    {
+        IEnumerable<Tile> activeTiles = from tile in tiles where tile.gameObject.activeSelf select tile;
+        foreach (Tile singleTile in activeTiles)
+        {
+            singleTile.gameObject.SetActive(false);
+        }
+
+        for (int rowIndex = rowIndexMin; rowIndex <= rowIndexMax; ++rowIndex)
+        {
+            for (int colIndex = colIndexMin; colIndex <= colIndexMax; ++colIndex)
+            {
+                MultiLayerTile multiTiles = FetchTile(rowIndex, colIndex);
+                for (int multiIndex = 0; multiIndex < multiTiles.multiLayerTile.Count; ++multiIndex)
+                {
+                    GameObject goTile = multiTiles.multiLayerTile[multiIndex];
+                    goTile.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void ActivateTiles(Vector3 topLeftPos, Vector3 bottomRightPos)
+    {
+        Vector2 topLeftIndex = FetchTileIndex(topLeftPos);
+        Vector2 bottomRightIndex = FetchTileIndex(bottomRightPos);
+        ActivateTiles((int)topLeftIndex.x, (int)bottomRightIndex.x, (int)topLeftIndex.y, (int)bottomRightIndex.y);
+    }
 
 	private bool loadFile()
 	{
@@ -91,9 +175,12 @@ public class TileMap : MonoBehaviour
 	private bool generateMap(ArrayList sMap, int numRow, int numCol)
 	{
 		TileSize = calculateTileSize();
+        tileMapDistToTopLeft = generateDistToTopLeft(numRow, numCol);
+        rowCount = numRow;
+        colCount = numCol;
 
-		// Calculate data needed
-		Vector3 size = new Vector3(TileSize, TileSize, 1); // Size of tile (Scale)
+        // Calculate data needed
+        Vector3 size = new Vector3(TileSize, TileSize, 1); // Size of tile (Scale)
 		Vector3 startPos = generateStartPos(numRow, numCol); // Calculate start position
 
 		// Generate map
@@ -103,7 +190,8 @@ public class TileMap : MonoBehaviour
 			string line = (string)sMap[rowIndex];
 			string[] tokens = line.Split(TILE_SPLIT); // Split each tile with ','
 
-			ArrayList rowOfData = new ArrayList(); // One row of tile
+            //ArrayList rowOfData = new ArrayList(); // One row of tile
+            Row rowOfData = new Row();
 
 			for (int colIndex = 0; colIndex < numCol;) // Loop for cols
 			{
@@ -113,8 +201,9 @@ public class TileMap : MonoBehaviour
                     // Split different layer tiles
                     string combinedLayerTiles = tokens[colIndex];
                     string[] layers = combinedLayerTiles.Split(TILE_MULTIPLE_LAYER_SPLIT);
-                    
-                    ArrayList multiLayerTile = new ArrayList();
+
+                    //ArrayList multiLayerTile = new ArrayList();
+                    MultiLayerTile multiLayerTile = new MultiLayerTile();
 
                     for (int layerIndex = 0; layerIndex < layers.Length; ++layerIndex)
                     {
@@ -122,29 +211,40 @@ public class TileMap : MonoBehaviour
                         tile = Instantiate(TileBlueprints[tileType]);
                         
                         // Set common data for each tile
-                        tile.SetActive(true);
+                        tile.SetActive(false);
                         tile.transform.position = startPos; // CHECK: Copy by value or reference
                         tile.transform.localScale = size; // CHECK: Copy by value or reference
+                        tile.transform.parent = this.transform;
 
                         // Add to multi-layer
-                        multiLayerTile.Add(tile);
+                        multiLayerTile.multiLayerTile.Add(tile);
+
+                        // Add to tile list
+                        tiles.Add(tile.GetComponent<Tile>());
                     }
 
                     // Add to row of data
-                    rowOfData.Add(multiLayerTile);
+                    rowOfData.column.Add(multiLayerTile);
                 }
 				else // Data not within file, empty tile
                 {
-                    ArrayList multiLayerTile = new ArrayList();
+                    MultiLayerTile multiLayerTile = new MultiLayerTile();
                     tile = Instantiate(TileBlueprints[(int)DefaultTile]);
 
                     // Set common data for each tile
-                    tile.SetActive(true);
+                    tile.SetActive(false);
                     tile.transform.position = startPos; // CHECK: Copy by value or reference
                     tile.transform.localScale = size; // CHECK: Copy by value or reference
+                    tile.transform.parent = this.transform;
 
-                    multiLayerTile.Add(tile);
-                    rowOfData.Add(multiLayerTile);
+                    // Add to multi-layer
+                    multiLayerTile.multiLayerTile.Add(tile);
+
+                    // Add to tile list
+                    tiles.Add(tile.GetComponent<Tile>());
+
+                    // Add to row of data
+                    rowOfData.column.Add(multiLayerTile);
 				}
 
 				// Next col startPos
@@ -240,6 +340,30 @@ public class TileMap : MonoBehaviour
 
 		return startPos;
 	}
+
+    private Vector3 generateDistToTopLeft(int numRow, int numCol)
+    {
+        Vector3 topLeft = Vector3.zero;
+        switch (TileMapOrigin)
+        {
+            case TILEMAP_ORIGIN.TILEMAP_TOP_LEFT:
+                {
+                    // Do nothing
+                }
+                break;
+            case TILEMAP_ORIGIN.TILEMAP_CENTER:
+                {
+                    topLeft.Set(numCol * TileSize * 0.5f, -(numRow * TileSize * 0.5f), 0.0f);
+                }
+                break;
+            case TILEMAP_ORIGIN.TILEMAP_BOTTOM_LEFT:
+                {
+                    topLeft.Set(0.0f, -(numRow * TileSize), 0.0f);
+                }
+                break;
+        }
+        return topLeft;
+    }
 
 	private int calculateTileSize()
 	{
