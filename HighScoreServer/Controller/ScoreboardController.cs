e00491c9bae49abfecde4701ee5958125c1d7441
Kah Wei <xeclipse120@live.com>
2016-02-20 22:28:06 +0800

    Added basic HighScoreServer implemented in ASP.NET. Currently, it is web-based with no Web API.

---
+++ HighScoreServer/Controller/ScoreboardController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using HighScoreServer.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HighScoreServer.Controllers
+{
+    public class ScoreboardController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Submit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Submit(ScoreEntry entry)
+        {
+            // Check if the result is valid
+            if (!ModelState.IsValid)
+            {
+                return View(entry);
+            }
+
+            // Save the entry asynchronously
+            var db = new ScoreDataContext();
+            db.Add(entry);
+            await db.SaveChangesAsync();
+
+            // Redirect to the Entry Action with an Anonymous Parameter that Entry() needs
+            return RedirectToAction("Entry", new { id = entry.Id });
+        }
+
+        public IActionResult Entry(int id)
+        {
+            // Look in the Database
+            var db = new ScoreDataContext();
+
+            // Retrieve the score entry using the ID (via LINQ lambda syntax)
+            ScoreEntry score = db.Entries.SingleOrDefault(x => id == x.Id);
+
+            return View(score);
+        }
+    }
+}
