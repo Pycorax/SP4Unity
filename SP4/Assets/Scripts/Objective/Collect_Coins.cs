c4d9c280d018041472c83f9e99daeacf056f2797
zlightningzap <wongdaokang@gmail.com>
2016-02-21 00:14:15 +0800

    - Changed both objectives recently pushed to be more consistent

--- SP4/Assets/Scripts/Objective/Collect_Coins.cs
+++ SP4/Assets/Scripts/Objective/Collect_Coins.cs
@@ -5,14 +5,15 @@ public class Collect_Coins : Objectives
 {
     private int coins = 0;
     public int requiredCoins = 50;
- 
-    public override bool IsComplete() 
+
+    public override bool IsAchieved() 
     {
         return (coins >= requiredCoins);
     }
  
     public override void Complete() 
     {
+
     }
  
     public void OnTriggerEnter2D(Collider2D other) 
--- SP4/Assets/Scripts/Objective/Kill_Enemy.cs
+++ SP4/Assets/Scripts/Objective/Kill_Enemy.cs
@@ -3,22 +3,25 @@ using System.Collections;
 
 public class Kill_Enemy : Objectives
 {
+    private int enemies = 0;
     public int EnemiesToKill = 10;
-    RPGPlayer player;
-    private int enemiesReset;
-
-    void Awake()
-    {
-        enemiesReset = player.EnemyKilled + EnemiesToKill;
-    }
 
     public override bool IsAchieved()
     {
-        return (player.EnemyKilled >= enemiesReset);
+        return (EnemiesToKill >= enemies);
     }
 
     public override void Complete()
     {
 
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (string.Equals(other.tag, "Enemy"))
+        {
+            enemies++;
+            Destroy(other.gameObject);
+        }
     }
 }
