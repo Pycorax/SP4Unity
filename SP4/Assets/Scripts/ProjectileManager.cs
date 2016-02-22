5824aa45ec2942f4c59a4113128c65b5d1fcf6bd
NothingMuch123 <curious_boii@hotmail.com>
2016-02-22 13:05:33 +0800

    Added different projectile list for different types of projectile

--- SP4/Assets/Scripts/ProjectileManager.cs
+++ SP4/Assets/Scripts/ProjectileManager.cs
@@ -2,30 +2,91 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ProjectileManager : MonoBehaviour {
-
+public class ProjectileManager : MonoBehaviour
+{
+    // Arrow
+    [Tooltip("Arrow projectile blueprint")]
     public GameObject ArrowBlueprint;
+    [Tooltip("Arrow default pool size")]
     public int ArrowPoolSize = 20;
     private List<GameObject> ArrowPool = new List<GameObject>();
 
-    
+    // Empowered Arrow
+    [Tooltip("Empowered Arrow projectile blueprint")]
+    public GameObject EmpoweredArrowBlueprint;
+    [Tooltip("Empowered Arrow default pool size")]
+    public int EmpoweredArrowPoolSize = 2;
+    private List<GameObject> EmpoweredArrowPool = new List<GameObject>();
 
-	// Use this for initialization
-	void Start () {
+    // Flying Sword
+    [Tooltip("Flying Sword projectile blueprint")]
+    public GameObject FlyingSwordBlueprint;
+    [Tooltip("Flying Sword default pool size")]
+    public int FlyingSwordPoolSize = 2;
+    private List<GameObject> FlyingSwordPool = new List<GameObject>();
+
+    // Lightning
+    [Tooltip("Lightning projectile blueprint")]
+    public GameObject LightningBlueprint;
+    [Tooltip("Lightning default pool size")]
+    public int LightningPoolSize = 20;
+    private List<GameObject> LightningPool = new List<GameObject>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        createArrow(ArrowPoolSize);
+        createEmpoweredArrow(EmpoweredArrowPoolSize);
+        createFlyingSword(FlyingSwordPoolSize);
+        createLightning(LightningPoolSize);
+	}
 	
-        for(int i = 0; i <  ArrowPoolSize; i++)
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    private void createArrow(int size)
+    {
+        for (int i = 0; i < size; i++)
         {
             GameObject arrow = Instantiate(ArrowBlueprint);
             arrow.SetActive(false);
             ArrowPool.Add(arrow);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    
+    }
+
+    private void createEmpoweredArrow(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            GameObject ea = Instantiate(EmpoweredArrowBlueprint);
+            ea.SetActive(false);
+            EmpoweredArrowPool.Add(ea);
+        }
+    }
+
+    private void createFlyingSword(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            GameObject fs = Instantiate(FlyingSwordBlueprint);
+            fs.SetActive(false);
+            FlyingSwordPool.Add(fs);
+        }
+    }
+
+    private void createLightning(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            GameObject lightning = Instantiate(LightningBlueprint);
+            lightning.SetActive(false);
+            LightningPool.Add(lightning);
+        }
+    }
+
     public GameObject FetchArrow()
     {
         foreach(GameObject arrow in ArrowPool)
@@ -33,6 +94,42 @@ public class ProjectileManager : MonoBehaviour {
             if(!arrow.activeSelf)
             {
                 return arrow;
+            }
+        }
+        return null;
+    }
+
+    public GameObject FetchEmpoweredArrow()
+    {
+        foreach(GameObject ea in EmpoweredArrowPool)
+        {
+            if (!ea.activeSelf)
+            {
+                return ea;
+            }
+        }
+        return null;
+    }
+
+    public GameObject FetchFlyingSword()
+    {
+        foreach (GameObject fs in FlyingSwordPool)
+        {
+            if (!fs.activeSelf)
+            {
+                return fs;
+            }
+        }
+        return null;
+    }
+
+    public GameObject FetchLightning()
+    {
+        foreach (GameObject lightning in LightningPool)
+        {
+            if (!lightning.activeSelf)
+            {
+                return lightning;
             }
         }
         return null;
