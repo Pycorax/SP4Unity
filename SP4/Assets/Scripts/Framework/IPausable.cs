73833f36d698a20ba7f612f708a7f4fd0c7456af
Kah Wei <msntkw@msn.com>
2016-02-22 18:20:36 +0800

    Added an IPausable interface for items that should be pausable as well as a PausableResourceManager with support for pausing items in the list.

---
+++ SP4/Assets/Scripts/Framework/IPausable.cs
@@ -0,0 +1,11 @@
+﻿public interface IPausable
+{
+    /// <summary>
+    /// Define what to do when this object is to be paused.
+    /// </summary>
+    void Pause();
+    /// <summary>
+    /// Define what to do when this object is to be unpaused.
+    /// </summary>
+    void Unpause();
+}
---
+++ SP4/Assets/Scripts/Framework/PausableResourceManager.cs
@@ -0,0 +1,25 @@
+﻿public class PausableResourceManager<T>  : ResourceManager<T>
+    where T : IResourceManagable, IPausable, new()
+{
+    /// <summary>
+    /// Pauses all the objects in the Resource List
+    /// </summary>
+    public void Pause()
+    {
+        foreach (var item in resourceList)
+        {
+            item.Pause();
+        }
+    }
+
+    /// <summary>
+    /// Unpauses all the objects in the Resource List
+    /// </summary>
+    public void Unpause()
+    {
+        foreach (var item in resourceList)
+        {
+            item.Unpause();
+        }
+    }
+}
