0af82d377c30d3196226a98f529cc06b733bcaeb
NothingMuch123 <curious_boii@hotmail.com>
2016-02-23 18:01:54 +0800

    LevelEditor script modify canvas scaler reference resolution to screen size. Added some UI gameobjects in LevelEditor scene for testing

---
+++ SP4/Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelEditor : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<CanvasScaler>().referenceResolution = ScreenData.GetScreenSize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
