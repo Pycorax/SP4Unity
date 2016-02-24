54d0c5b2c5ace277d737d4f9284bfe55ae848b56
Wayne Lee <yuckar97@gmail.com>
2016-02-24 17:09:15 +0800

    [ADDED] OptionScene, BGMScript, SFXScript

---
+++ SP4/Assets/Scripts/Sound/BGMScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMScript : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeVolume(float volume)
+    {
+        Mathf.Clamp(volume, 0f, 1f);
+        SoundManager.BGMVolume = volume;
+    }
+}
