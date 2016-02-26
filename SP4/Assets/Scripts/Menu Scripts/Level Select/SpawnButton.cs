621f665b0e6be3dca450e7ac096c2e6b98fffd74
Wayne Lee <yuckar97@gmail.com>
2016-02-26 17:58:18 +0800

    I did stuff ok

---
+++ SP4/Assets/Scripts/Menu Scripts/Level Select/SpawnButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class SpawnButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var info = new DirectoryInfo("Assets/Scenes/GameScenes");
+        var files = info.GetFiles();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
