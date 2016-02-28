8fa8b9fd9a04603bbc006f45aae8e6077a5937bb
Kah Wei <xeclipse120@live.com>
2016-02-28 22:12:33 +0800

    Replaced existing UI Hero Bar with Ribbon implementation

---
+++ SP4/Assets/Scripts/Level Editor/LevelEditorRibbonController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class LevelEditorRibbonController : MonoBehaviour
+{
+    public enum RibbonViews
+    {
+        Objective,
+        View
+    }
+
+    [Tooltip("The default panel to show.")]
+    public RibbonViews DefaultView = RibbonViews.Objective;
+    [Tooltip("Stores the references to the Ribbon panels")]
+    public GameObject[] RibbonPanels = new GameObject[Enum.GetNames(typeof(RibbonViews)).Length];
+
+
+    void Start()
+    {
+        switchTo(DefaultView);
+    }
+
+    public void SwitchToObjectives()
+    {
+        switchTo(RibbonViews.Objective);
+    }
+
+    public void SwitchToViews()
+    {
+        switchTo(RibbonViews.View);
+    }
+
+    private void switchTo(RibbonViews view)
+    {
+        disableAllPanels();
+
+        // Activate the panel
+        RibbonPanels[(int)view].SetActive(true);
+    }
+
+    private void disableAllPanels()
+    {
+        foreach (var panel in RibbonPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+}
