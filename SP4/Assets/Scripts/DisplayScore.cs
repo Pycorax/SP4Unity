5b0aa0893e462c39fbe8473ce47f4137f7e2275e
zlightningzap <wongdaokang@gmail.com>
2016-03-01 16:22:17 +0800

    - Added score display to the game scene

---
+++ SP4/Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayScore : MonoBehaviour
+{
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set up the reference.
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = ScoreManager.CurrentScore.ToString();
+    }
+}
