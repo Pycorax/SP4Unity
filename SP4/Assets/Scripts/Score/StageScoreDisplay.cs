e0e491cf0d886db6606e7af3697352ca5ba2bf0e
Wayne Lee <yuckar97@gmail.com>
2016-03-01 21:13:43 +0800

    [ADDED] Display Score on Win/Lose Scene, Buttons transition to the related Scenes now

---
+++ SP4/Assets/Scripts/Score/StageScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StageScoreDisplay : MonoBehaviour {
+
+    public Text text;
+
+    private int score;
+    
+	// Use this for initialization
+	void Start () {
+
+        //text = GetComponent<Text>();
+
+        score = ScoreManager.CurrentScore;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = score.ToString();
+	}
+}
