0e6a11f98ff6483c0f7f6ca69fe56e0de6ce65a4
zlightningzap <wongdaokang@gmail.com>
2016-03-02 17:24:41 +0800

    - Objective text now scale with text size - Gamescene Scoring is now bigger then ever

--- SP4/Assets/Scripts/Objective/PrintObjective.cs
+++ SP4/Assets/Scripts/Objective/PrintObjective.cs
@@ -6,6 +6,7 @@ public class PrintObjective : MonoBehaviour
 {
     public GameManager manager;
     Text text;
+    private int objectivetextsize;
 
     // Use this for initialization
     void Start()
@@ -17,6 +18,17 @@ public class PrintObjective : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = manager.CurrentObjective.GetDescription().ToString();
+        objectivetextsize = manager.CurrentObjective.GetDescription().ToString().Length;
+
+        if(objectivetextsize >= 25)
+        {
+            text.fontSize = 20;
+            text.text = manager.CurrentObjective.GetDescription().ToString();
+        }
+        else if(objectivetextsize <=24)
+        {
+            text.fontSize = 25;
+            text.text = manager.CurrentObjective.GetDescription().ToString();
+        }
     }
 }
