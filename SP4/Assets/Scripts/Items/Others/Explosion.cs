7643419bed45f07d94430b501d89169d1245019e
Kah Wei <msntkw@msn.com>
2016-03-03 15:43:18 +0800

    Fixed explosion animation

--- SP4/Assets/Scripts/Items/Others/Explosion.cs
+++ SP4/Assets/Scripts/Items/Others/Explosion.cs
@@ -23,36 +23,12 @@ public class Explosion : MonoBehaviour
     {
         // Set up Components
         collider = GetComponent<Collider2D>();
-
-        // Initialize the size
-        transform.localScale = StartSize;
-
-        // Initialize the explosion "direction"
-        expandDelta = MaxSize - StartSize;
-        if (expandDelta != Vector2.zero)
-        {
-            expandDelta.Normalize();
-        }
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    public void Disable()
     {
-	    if (transform.localScale.x < MaxSize.x && transform.localScale.y < MaxSize.y)
-        {
-            Vector2 newScale = transform.localScale;
-            newScale += expandDelta * BoomSpeed * (float)TimeManager.GetDeltaTime(TimeManager.TimeType.Game);
-            transform.localScale = newScale;
-        }
-        else
-        {
-            // Once we become large enough, it's time to kill ourself
-            transform.gameObject.SetActive(false);
-
-            // Reset variables
-            transform.localScale = StartSize;
-        }
-	}
+        gameObject.SetActive(false);
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
